938031f95f621701cbfd759f269cc121af64b80f
merapi <marcin@webskill.pl>
2021-09-10 13:05:34 +0200

    More metrics

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import ReactGA from 'react-ga';
 
 function App() {
     const sendTiming = () => {
-        const perfData = window.performance.timing;
-        const perfDataV2 = window.performance.getEntriesByType("navigation")[0];
-        const pageLoadTime = perfData.loadEventEnd - perfData.navigationStart;
-        const data = {
+        const perfData = window.performance.getEntriesByType("navigation")[0];
+        ReactGA.timing({
             category: 'Metrics',
             variable: 'load',
-            value: pageLoadTime,
+            value: perfData.domComplete,
             label: 'Load time'
-        }
-        ReactGA.timing(data);
+        });
+        ReactGA.timing({
+            category: 'Metrics',
+            variable: 'domInteractive',
+            value: perfData.domInteractive,
+        });
         ReactGA.event({
             category: 'User',
             action: 'Button Click',
             label: 'Send metrics to GA',
             value: 1,
         });
-        console.log(data)
-        console.log({perfData, perfDataV2})
     }
   return (
     <div className="App">
